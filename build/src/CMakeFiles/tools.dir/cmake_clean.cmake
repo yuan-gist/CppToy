e7eb16c31fd6e5fb1ebb5b0a314880ed6eda5883
Y <zkyuans@hotmail.com>
2022-04-03 21:45:15 +0200

    Revert "Revert "opencv examples""
    This reverts commit d94ae84ee90ec1aa8bb2bb28c28705c90346065a.

---
+++ build/src/CMakeFiles/tools.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../lib/libtools.a"
+  "../lib/libtools.pdb"
+  "CMakeFiles/tools.dir/add.cpp.o"
+  "CMakeFiles/tools.dir/add.cpp.o.d"
+  "CMakeFiles/tools.dir/say_hello.cpp.o"
+  "CMakeFiles/tools.dir/say_hello.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CMakeFiles/runUnitTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/runUnitTests"
+  "../bin/runUnitTests.pdb"
+  "CMakeFiles/runUnitTests.dir/example.cpp.o"
+  "CMakeFiles/runUnitTests.dir/example.cpp.o.d"
+  "CMakeFiles/runUnitTests.dir/test_add.cpp.o"
+  "CMakeFiles/runUnitTests.dir/test_add.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runUnitTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
