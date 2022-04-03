c8df6ae9ff8585f0fefc0b15054ba80dfa9653ae
Y <zkyuans@hotmail.com>
2022-04-03 21:45:56 +0200

    opencv examples: remove build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yuan/Documents/C++/mySmallProject
-# Build directory: /Users/yuan/Documents/C++/mySmallProject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("_deps/googletest-build")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yuan/Documents/C++/mySmallProject/tests
-# Build directory: /Users/yuan/Documents/C++/mySmallProject/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runUnitTests "/Users/yuan/Documents/C++/mySmallProject/build/bin/runUnitTests")
-set_tests_properties(runUnitTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/yuan/Documents/C++/mySmallProject/tests/CMakeLists.txt;5;add_test;/Users/yuan/Documents/C++/mySmallProject/tests/CMakeLists.txt;0;")
