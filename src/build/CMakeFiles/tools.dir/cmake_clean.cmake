ce4873e9a85c9c4e97f8b970bec165aaad88ca78
Y <zkyuans@hotmail.com>
2022-04-03 21:48:10 +0200

    remove unecessary files

---
+++ src/build/CMakeFiles/tools.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tools.dir/add.cpp.o"
+  "CMakeFiles/tools.dir/add.cpp.o.d"
+  "CMakeFiles/tools.dir/say_hello.cpp.o"
+  "CMakeFiles/tools.dir/say_hello.cpp.o.d"
+  "libtools.a"
+  "libtools.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
